090eca444e06e0f2867eeb05fe526d792a5ee155
Valeri Asmus <vaasit00@hs-esslingen.de>
2015-06-15 11:04:04 +0200

    AirportFacadeBean um showWeather() erweitert. Index.xhtml  Button für ShowWeather eingefügt

--- src/workspace/AirportWEB/src/de/airport/gui/AirportFacadeBean.java
+++ src/workspace/AirportWEB/src/de/airport/gui/AirportFacadeBean.java
@@ -1,5 +1,13 @@
 package de.airport.gui;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.ejb.EJB;
@@ -16,10 +24,61 @@ public class AirportFacadeBean {
 	private String name;
 	private String street;
 	private String city;
+	//fuer das wetteranzeige
+	private String temperatur;
+	private String wetterStatus;
+	private String windstaerke;
+	private String luftfeuchtigkeit;
+	private String regenwahrscheinlichkeit;
+	
+	//aktuelle Uhrzeit und Datum 
+	//private String ADateAndTime;
+	
 	
 	@EJB
 	private AirportFacade facade;
 
+	/*public String getADateAndTime() {
+		setADateAndTime();
+		return ADateAndTime;
+	}
+
+	public void setADateAndTime() {
+		java.util.Date now = new java.util.Date();
+		java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("dd.MM.yyyy HH.mm.ss");
+		this.ADateAndTime = sdf.format(now);
+	}*/
+	int start = 0;
+	int end = 0;
+	public void showWeather(){
+		
+		URL weather;
+		try {
+			weather = new URL("http://www.webservicex.net/globalweather.asmx/GetWeather?CityName=Stuttgart&CountryName=Germany");
+		
+        URLConnection yc = weather.openConnection();
+        BufferedReader in = new BufferedReader(
+                                new InputStreamReader(
+                                yc.getInputStream()));
+        String inputLine;
+        in.readLine();
+        in.readLine();
+        while ((inputLine = in.readLine()) != null){ 
+        if(inputLine.matches("[CurrentWeather]+") ){
+        start = inputLine.indexOf("&gt;")+2;
+        end = inputLine.indexOf("&lt;",4)-2;
+        System.out.print("Start " + start + "Ende "+ end);
+        System.out.println(inputLine);
+    //    System.out.println(inputLine.substring(start, end));
+        	}    
+        }
+        in.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	public String getStreet() {
 		return street;
 	}
@@ -60,6 +119,8 @@ public class AirportFacadeBean {
 	public List<Airplane> getAirplanes() {
 		return facade.getAirplanes();
 	}
+
+	
 	
 	
 }
